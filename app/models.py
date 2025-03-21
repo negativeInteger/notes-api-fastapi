f34bd02e0e28cc30d23e257e64ce2bc9ef66c200
negativeInteger <139580532+INFERNO1211@users.noreply.github.com>
2025-03-21 23:21:56 +0530

    crud routes done✅

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from uuid import UUID
+from uuid import uuid4
 
 class Note(BaseModel):
-    id: UUID
+    id: str = Field(default_factory=lambda: str(uuid4()))
     title: str = Field(..., min_length=1, max_length=100)
-    description: Optional[str] = Field(default=None, max_length=1000)
+    description: Optional[str] = Field(default='no description provided yet', max_length=1000)
+    
+class CreateNote(BaseModel):
+    title: str = Field(..., min_length=1, max_length=100)
+    description: Optional[str] = Field(default='no description provided yet', max_length=1000)
+    
+class UpdateNote(BaseModel):
+    title: Optional[str] = Field(..., min_length=1, max_length=100)
+    description: Optional[str] = Field(default='no description provided yet', max_length=1000)
     
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,82 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Response
+from app.models import Note, CreateNote
+from app.storage import read_notes, write_notes
 
 # create a router instance
 router = APIRouter()
 
-@router.get('/ping')
-def test():
-    return {"message": "HELLO EVERYTHHING WORKING"}
+# Get all notes
+@router.get('/notes', response_model=list[Note])
+def get_notes():
+    return read_notes()
+
+# Create a new note
+@router.post('/notes', response_model=Note, status_code=status.HTTP_201_CREATED)
+def create_note(note: CreateNote):
+    notes = read_notes()
+    
+    new_note = Note(
+        title = note.title,
+        description = note.description
+    )
+    
+    notes.append(new_note)
+    write_notes(notes)
+    return new_note
+
+# Get note by ID
+@router.get('/notes/{note_id}', response_model=Note)
+def get_note(note_id: str):
+    notes = read_notes()
+    
+    for note in notes:
+        if note.id == note_id:
+            return note
+        
+    raise HTTPException(
+        status_code=404,
+        detail='no note with this ID'
+    )
+
+# Update note by ID
+@router.put('/notes/{note_id}', response_model=Note)
+def update_note(note_id: str, updated_note: CreateNote):
+    notes = read_notes()
+    
+    if not updated_note.title and not updated_note.description:
+        raise HTTPException(
+            status_code=400,
+            detail='At least one property must be updated'
+        )
+    
+    for note in notes:
+        if note.id == note_id:
+            if updated_note.title:
+                note.title = updated_note.title
+            if updated_note.description:
+                note.description = updated_note.description
+            write_notes(notes)
+            return Note(
+                id = note_id,
+                title = updated_note.title,
+                description = updated_note.description
+            )
+    
+    raise HTTPException(
+        status_code=404,
+        detail='no note with this ID'
+    )
+    
+# Delete note by ID
+@router.delete('/notes/{note_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_note(note_id: str):
+    notes = read_notes()
+    for note in notes:
+        if note.id == note_id:
+            notes.remove(note)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)  
+        
+    raise HTTPException(status_code=404, detail="Note not found")
+    
+    
+    
